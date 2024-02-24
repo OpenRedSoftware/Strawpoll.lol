eebe7c5ff5cebe0a09fc6502f966efb67b537033
dginovker <danielginovker@gmail.com>
2024-02-24 15:06:13 -0500

    Improve UX a lot

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+export
+  const firebaseConfig = {
+    apiKey: "AIzaSyAshR4OaNmcBGBRvMTsUz2VP7u6pLFCKqA",
+    authDomain: "strawpolllol.firebaseapp.com",
+    databaseURL: "https://strawpolllol-default-rtdb.firebaseio.com",
+    projectId: "strawpolllol",
+    storageBucket: "strawpolllol.appspot.com",
+    messagingSenderId: "955168381009",
+    appId: "1:955168381009:web:45a077fd593c3fd69b0070"
+  };
